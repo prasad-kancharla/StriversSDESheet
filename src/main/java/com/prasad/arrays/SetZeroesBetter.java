c322fb17f3eef59949982e4ece92e08862b22c88
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-03-04 22:10:47 +0530

    Create SetZeroesBetter.java

---
+++ src/main/java/com/prasad/arrays/SetZeroesBetter.java
@@ -0,0 +1,30 @@
+package com.prasad.arrays;
+
+public class SetZeroesBetter {
+
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] rows = new int[n];
+        int[] columns = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    columns[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] == 1 || columns[i] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
