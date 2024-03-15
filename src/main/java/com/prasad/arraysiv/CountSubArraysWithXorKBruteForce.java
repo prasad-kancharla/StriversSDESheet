2200001a184d60814cb66a565551478f6b86d080
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-03-15 19:53:12 +0530

    Refactor code

--- src/main/java/com/prasad/arraysiv/CountSubArraysWithXorKBruteForce.java
+++ src/main/java/com/prasad/arraysiv/CountSubArraysWithXorKBruteForce.java
@@ -3,16 +3,18 @@ package com.prasad.arraysiv;
 public class CountSubArraysWithXorKBruteForce {
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 3, 2};
-        System.out.println(subarraysWithSumK(input, 2));
+        int[] input = {1, 3, 3, 3, 5};
+        System.out.println(subarraysWithSumK(input, 6));
     }
-    public static int subarraysWithSumK(int []a, int b) {
+
+    public static int subarraysWithSumK(int[] a, int b) {
         int n = a.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (getXor(a, i, j) == b) {
                     count++;
+//                    System.out.println("start: " + i + ", end: " + j);
                 }
             }
         }
@@ -21,7 +23,7 @@ public class CountSubArraysWithXorKBruteForce {
 
     private static int getXor(int[] a, int i, int j) {
         int xor = 0;
-        for (int k = i; k <= j ; k++) {
+        for (int k = i; k <= j; k++) {
             xor = xor ^ a[k];
         }
         return xor;
