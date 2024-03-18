6c144159e0cb0b71cdbf3ae7afaea4af846740ef
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-03-18 17:51:09 +0530

    Solve Detect cycle using floyds algorithm

---
+++ src/main/java/com/prasad/linkedlistii/DetectCycleOptimal.java
@@ -0,0 +1,15 @@
+package com.prasad.linkedlistii;
+
+public class DetectCycleOptimal {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
