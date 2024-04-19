6cec2ebd74c096e17d5d54eec0cb32ee4cf84dc1
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-04-19 11:53:13 +0530

    Add linkedlist node

---
+++ src/main/java/com/prasad/companies/epam/LinkedListNode.java
@@ -0,0 +1,12 @@
+package com.prasad.companies.epam;
+
+class LinkedListNode<T> {
+
+    T data;
+    LinkedListNode<T> next;
+
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+
+}
