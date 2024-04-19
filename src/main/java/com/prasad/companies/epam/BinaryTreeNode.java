2f2e16f04c78ce3a0cc706c5b1f0f4d5fd9b4c42
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-04-19 23:42:44 +0530

    Add BinaryTree node

---
+++ src/main/java/com/prasad/companies/epam/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package com.prasad.companies.epam;
+
+class BinaryTreeNode<Integer> {
+
+    int data;
+    BinaryTreeNode<Integer> left;
+    BinaryTreeNode<Integer> right;
+
+    public BinaryTreeNode(int data) {
+        this.data = data;
+    }
+
+}
