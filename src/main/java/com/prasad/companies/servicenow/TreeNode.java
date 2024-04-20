7ef843da37707b43d569eeced5e91e4c2745cdef
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-04-20 22:13:59 +0530

    Add Treenode

---
+++ src/main/java/com/prasad/companies/servicenow/TreeNode.java
@@ -0,0 +1,15 @@
+package com.prasad.companies.servicenow;
+
+class TreeNode<T> {
+
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+};
