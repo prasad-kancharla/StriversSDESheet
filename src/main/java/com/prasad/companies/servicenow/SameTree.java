e051c2da87b8fb95774a3bb38117f448bc5c9736
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-04-27 23:20:11 +0530

    Solve same tree

---
+++ src/main/java/com/prasad/companies/servicenow/SameTree.java
@@ -0,0 +1,34 @@
+package com.prasad.companies.servicenow;
+
+class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
