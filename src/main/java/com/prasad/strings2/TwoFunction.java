e531905c671dd868b9e29a9760c36c366da0f509
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-05-07 22:44:07 +0530

    Solve Z function

---
+++ src/main/java/com/prasad/strings2/TwoFunction.java
@@ -0,0 +1,17 @@
+package com.prasad.strings2;
+
+class TwoFunction {
+
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
