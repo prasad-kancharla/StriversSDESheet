877ae68b884dfab511906fa294b9a2c1baf31386
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-05-31 15:36:54 +0530

    Solve LCA of BST

---
+++ src/main/java/com/prasad/binarysearchtree/LCAofBST.java
@@ -0,0 +1,20 @@
+package com.prasad.binarysearchtree;
+
+class LCAofBST {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == root || q == root) {
+            return root;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+}
