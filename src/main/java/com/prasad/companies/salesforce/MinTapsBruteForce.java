8c40756600c21614bf09f3c47ea2f5cec848af79
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-06-20 22:28:19 +0530

    Solve Min Taps Brute force

---
+++ src/main/java/com/prasad/companies/salesforce/MinTapsBruteForce.java
@@ -0,0 +1,68 @@
+package com.prasad.companies.salesforce;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/description/
+class MinTapsBruteForce {
+
+    public int minTaps(int n, int[] ranges) {
+        List<Range> rangeList = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            int start = Math.max(i - ranges[i],0);
+            int end = Math.min(i + ranges[i], n);
+            rangeList.add(new Range(start, end));
+        }
+        Comparator<Range> rangeComparator = Comparator.comparingInt(Range::getStart).thenComparingInt(Range::getEnd);
+        Collections.sort(rangeList, rangeComparator);
+        int minTaps = getTapsCount(0, 0, rangeList);
+        if (minTaps == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return minTaps;
+    }
+
+    private int getTapsCount(int i, int maxEnd, List<Range> rangeList) {
+        if (maxEnd >= rangeList.size() - 1) {
+            return 0;
+        }
+        if (i >= rangeList.size()) {
+            return Integer.MAX_VALUE;
+        }
+        Range range = rangeList.get(i);
+        if (range.start > maxEnd) {
+            return Integer.MAX_VALUE;
+        }
+        int count1 = getTapsCount(i + 1, maxEnd, rangeList);
+        int count2 = Integer.MAX_VALUE;
+        if (range.end > maxEnd) {
+            int nextCount = getTapsCount(i + 1, range.end, rangeList);
+            count2 = nextCount != Integer.MAX_VALUE ? 1 + nextCount : Integer.MAX_VALUE;
+        }
+
+        return Math.min(count1, count2);
+    }
+
+    class Range {
+
+        int start;
+        int end;
+
+        Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        int getStart() {
+            return start;
+        }
+
+        int getEnd() {
+            return end;
+        }
+
+    }
+
+}
