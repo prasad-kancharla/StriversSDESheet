58295b17d8c260004225771b93eca27307380e42
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2024-06-29 18:14:49 +0530

    Refactor code

--- src/main/java/com/prasad/companies/google/CountUnreachablePairsBruteForce.java
+++ src/main/java/com/prasad/companies/google/CountUnreachablePairsBruteForce.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-class CountUnreachablePairOfNodesBruteForce {
+class CountUnreachablePairsBruteForce {
 
     int[] visited;
 
